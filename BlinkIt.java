c9192b7759b3d06efb615c06719373bfd7557cd9
Sumamallikar <sumakm942002@gmail.com>
2024-10-07 14:42:44 +0530

    BlickIt createde

---
+++ BlinkIt.java
@@ -0,0 +1,13 @@
+abstract class BlinkIt
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to BlinkIt");
+	}
+	public void displayAllProducts()
+	{
+		System.out.println("All products are here");
+	}
+	public void addProductToCart(int id);
+}
+		
